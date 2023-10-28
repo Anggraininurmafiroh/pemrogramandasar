5331cef0599605e0b39243059f44da08198725a7
Anggraininurmafiroh <148602196+Anggraininurmafiroh@users.noreply.github.com>
2023-10-28 13:28:24 +0700

    Create latihan1_variabel.py

---
+++ latihan1_variabel.py
@@ -0,0 +1,44 @@
+print("nomor satu")
+a = 20
+b = 30
+c = 5
+
+hasil = a + b
+print(a,"+",b,"=",hasil)
+
+hasil = a - c
+print(a,"-",c,"=",hasil)
+print("=====================================================================")
+
+print("nomor dua")
+a = 20
+b = 30
+c = 5
+hasil = a * b / a + c
+print(a,"*",b,"/",a,"+",c,"=",hasil)
+print("======================================================================")
+
+print("nomor tiga, menghitung luas trapesium")
+print("rumus luas trapesium = 1/2 x jumlah panjang sisi sejajar x tinggi")
+print("L=1/2 x (a+b) x t")
+a = 20
+b = 30
+t = 40
+hasil = 1/2*(a+b)*t
+print(1,"/",2,"*",(a,"+",b),"*",t,"=",hasil)
+print("=======================================================================")
+
+print("nomor empat, keliling lingkaran")
+print("rumus keliling lingkaran = 2 x phi x r")
+phi = 22/7
+r = 7 
+hasil = 2*phi*7
+print(2,"*",phi,"*",7,"=",hasil)
+print("=======================================================================")
+
+print("nomor lima, luas lingkaran")
+print("rumus luas lingkaran = phi x r x r")
+phi = 22/7
+r = 7
+hasil = phi*r*r
+print(phi,"*",r,"*",r,"=",hasil)
